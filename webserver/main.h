6863d207796a314637af10473f785b063f908e8c
Goliath33 <gauliath@msn.com>
2024-03-05 07:29:14 +0100

    update

--- webserver/main.h
+++ webserver/main.h
@@ -11,6 +11,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
+// Inclusions de bibliothèques supplémentaires
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
@@ -20,25 +22,28 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-
+#include <dirent.h>
+#include <signal.h>
 
 // Definitions
 #define SERVER_PORT 80
-#define WEB_ROOT "/home/goliath/Project/Project_holbertonschool/webserver/web/"
+#define WEB_ROOT "/home/goliath/Project/Project_holbertonschool/webserver/web/holbertonschool/"
 #define BUFFER_SIZE 1024
 
 // Prototypes
-int main(void);
-void serve_php(int sockfd, char *file_path);
-void serve_http(int sockfd, struct sockaddr_in *client_addr_ptr);
-void serve_https(int sockfd, struct sockaddr_in *client_addr_ptr);
-void fatal(const char *message);
-void handle_request(int sockfd);
-void handle_proxy_request(int sockfd);
-void serve_file(int sockfd, char *file_path);
-void serve_php(int sockfd, char *file_path);
-void *thread_function(void *arg);
-void handle_connection(int sockfd, struct sockaddr_in *client_addr_ptr);
+int main(int argc, char const *argv[]);                                         //main appele le serveur web    (1er parametre)
+void serve_php(int sockfd, char *file_path);                                    //serveur php                   (2eme parametre)
+void serve_file(int sockfd, char *file_path);                                   //serveur file                  (3eme parametre)
+void serve_request(int sockfd, struct sockaddr_in *client_addr_ptr);            //serveur request               (4eme parametre)
+void serve_http(int sockfd, struct sockaddr_in *client_addr_ptr);               //serveur http                  (5eme parametre)
+void serve_https(int sockfd, struct sockaddr_in *client_addr_ptr);              //serveur https                 (6eme parametre)
+void fatal(const char *message);                                                //fatale message d'erreur       (7eme parametre)
+void handle_request(int sockfd);                                                //handle request                (8eme parametre)
+void handle_proxy_request(int sockfd);                                          //handle proxy request          (9eme parametre)
+void *thread_function(void *arg);                                               //thread function               (10eme parametre)
+void handle_connection(int sockfd, struct sockaddr_in *client_addr_ptr);        //handle connection             (11eme parametre)
+void serve_connection(int sockfd, struct sockaddr_in *client_addr_ptr);         //serve connection              (12eme parametre)
+void serve_connection_proxy(int sockfd, struct sockaddr_in *client_addr_ptr);   //serve connection proxy        (13eme parametre)
 
 
 #endif // MAIN_H
--- webserver/web.c
+++ webserver/web.c
@@ -1,10 +1,123 @@
 /**
  * @file web.c - webserver html + php
  * AUTHOR : GOLIATH
- * VERSION : 1.0.0
+ * VERSION : 1.1.0
  */
 
 #include "main.h"
+
+void serve_php(int sockfd, char *file_path)
+{
+    char buffer[BUFFER_SIZE];
+    ssize_t num_bytes;
+    while ((num_bytes = read(sockfd, buffer, BUFFER_SIZE)) > 0)
+        send(sockfd, buffer, num_bytes, 0);
+    close(sockfd);
+
+    return;
+}
+
+void serve_file(int sockfd, char *file_path)
+{
+    char buffer[BUFFER_SIZE];
+    ssize_t num_bytes;
+    while ((num_bytes = read(sockfd, buffer, BUFFER_SIZE)) > 0)
+        send(sockfd, buffer, num_bytes, 0);
+    close(sockfd);
+    return;
+}
+
+void serve_request(int sockfd, struct sockaddr_in *client_addr_ptr)
+{
+    char buffer[BUFFER_SIZE];
+    ssize_t num_bytes;
+    while ((num_bytes = read(sockfd, buffer, BUFFER_SIZE)) > 0)
+        send(sockfd, buffer, num_bytes, 0);
+    close(sockfd);
+    return;
+}
+
+void serve_http(int sockfd, struct sockaddr_in *client_addr_ptr)
+{
+    char request[BUFFER_SIZE];
+    ssize_t bytes_received = recv(sockfd, request, sizeof(request) - 1, 0);
+    if (bytes_received == -1)
+    {
+        char error_message[1024];
+        snprintf(error_message, sizeof(error_message), "recv: %s", strerror(errno));
+        perror(error_message);
+        return;
+    }
+
+    request[bytes_received] = '\0';
+    printf("Received request: %s\n", request);
+
+    int php_sockfd = socket(PF_INET, SOCK_STREAM, 0);
+    if (php_sockfd < 0)
+    {
+        perror("socket creation failed");
+        return;
+    }
+    struct sockaddr_in php_addr;
+    php_addr.sin_family = 2;
+    php_addr.sin_port = htons(9000);
+    php_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+    if (connect(php_sockfd, (struct sockaddr *)&php_addr, sizeof(php_addr)) == -1)
+    {
+        char error_message[1024];
+        snprintf(error_message, sizeof(error_message), "connect: %s", strerror(errno));
+        perror(error_message);
+        return;
+    }
+    if (send(php_sockfd, request, strlen(request), 0) == -1)
+    {
+        char error_message[1024];
+        snprintf(error_message, sizeof(error_message), "send: %s", strerror(errno));
+        perror(error_message);
+        return;
+    }
+
+    char response[BUFFER_SIZE];
+    ssize_t bytes_received_from_php = recv(php_sockfd, response, sizeof(response) - 1, 0);
+    if (bytes_received_from_php == -1)
+    {
+        char error_message[1024];
+        snprintf(error_message, sizeof(error_message), "recv: %s", strerror(errno));
+        perror(error_message);
+        return;
+    }
+    response[bytes_received_from_php] = '\0';
+
+    if (send(sockfd, response, strlen(response), 0) == -1)
+    {
+        char error_message[1024];
+        snprintf(error_message, sizeof(error_message), "send: %s", strerror(errno));
+        perror(error_message);
+        return;
+    }
+
+    close(sockfd);
+    close(php_sockfd);
+}
+
+void serve_https(int sockfd, struct sockaddr_in *client_addr_ptr)
+{
+    char buffer[BUFFER_SIZE];
+    ssize_t num_bytes;
+    while ((num_bytes = read(sockfd, buffer, BUFFER_SIZE)) > 0)
+        send(sockfd, buffer, num_bytes, 0);
+    close(sockfd);
+    return;
+    // TODO: implement https
+    // serve_file(sockfd, file_path);
+    // return;
+    // TODO: implement https
+    // serve_file(sockfd, file_path);
+    // return;
+
+    // serve_file(sockfd, file_path);
+    // return;
+}
 
 void fatal(const char *message)
 {
@@ -12,6 +125,7 @@ void fatal(const char *message)
     snprintf(error_message, sizeof(error_message), "%s: %s", message, strerror(errno));
     perror(error_message);
     exit(1);
+    return;
 }
 
 void handle_request(int sockfd)
@@ -19,82 +133,20 @@ void handle_request(int sockfd)
     pthread_t thread;
     pthread_create(&thread, NULL, thread_function, &sockfd);
     pthread_detach(thread);
+    return;
+
+    // TODO: implement proxy
 }
 
 void handle_proxy_request(int sockfd)
 {
     char buffer[BUFFER_SIZE];
-    ssize_t numbytes;
-    while ((numbytes = read(sockfd, buffer, BUFFER_SIZE)) > 0)
-        send(sockfd, buffer, numbytes, 0);
+    ssize_t num_bytes;
+    while ((num_bytes = read(sockfd, buffer, BUFFER_SIZE)) > 0)
+        send(sockfd, buffer, num_bytes, 0);
     close(sockfd);
-}
 
-void serve_file(int sockfd, char *file_path)
-{
-    char buffer[BUFFER_SIZE];
-    ssize_t numbytes;
-    while ((numbytes = read(sockfd, buffer, BUFFER_SIZE)) > 0)
-        send(sockfd, buffer, numbytes, 0);
-    close(sockfd);
-}
-
-void serve_http(int sockfd, struct sockaddr_in *client_addr_ptr)
-{
-    serve_file(sockfd, WEB_ROOT "http.php");
-}
-
-void serve_https(int sockfd, struct sockaddr_in *client_addr_ptr)
-{
-    serve_file(sockfd, WEB_ROOT "https.php");
-}
-
-void serve_php(int sockfd, char *file_path)
-{
-    char buffer[BUFFER_SIZE];
-    ssize_t numbytes;
-    while ((numbytes = read(sockfd, buffer, BUFFER_SIZE)) > 0)
-        send(sockfd, buffer, numbytes, 0);
-    close(sockfd);
-}
-
-int main(void)
-{
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) // Pas besoin de comparer avec -1 car si le socket n'est pas crée alors on rencontrera un échec.
-    {
-        fatal("socket creation failed");
-    }
-
-    struct sockaddr_in server_address;
-    memset(&server_address, 0, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(SERVER_PORT);
-
-    if (bind(sockfd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
-    {
-        fatal("binding failed");
-    }
-
-    if (listen(sockfd, 20) < 0) // Pas besoin de comparer avec -1 car si la connexion n'est pas établie alors on rencontrera un échec.
-    {
-        fatal("listening failed");
-    }
-
-    while (1)
-    {
-        struct sockaddr_in client_address;
-        socklen_t addr_size = sizeof(struct sockaddr_in);
-        int new_sockfd = accept(sockfd, (struct sockaddr *)&client_address, &addr_size); // Accepte la connexion du client.
-
-        if (new_sockfd < 0) // Si une erreur se produit pendant l'acceptation de la connexion.
-            perror("Accept failed");
-
-        handle_connection(new_sockfd, &client_address); // Gère cette nouvelle connexion.
-    }
-
-    close(sockfd); // Ferme le socket principal.
-    return 0;
+    return;
 }
 
 void *thread_function(void *arg)
@@ -119,7 +171,7 @@ void *thread_function(void *arg)
         return NULL;
     }
     struct sockaddr_in php_addr;
-    php_addr.sin_family = AF_INET;
+    php_addr.sin_family = 2;
     php_addr.sin_port = htons(9000);
     php_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
     if (connect(php_sockfd, (struct sockaddr *)&php_addr, sizeof(php_addr)) == -1)
@@ -193,4 +245,56 @@ void handle_connection(int sockfd, struct sockaddr_in *client_addr_ptr)
         send(sockfd, buffer, numbytes, 0);
     close(fd);
     close(sockfd);
+}
+
+void serve_connection(int sockfd, struct sockaddr_in *client_addr_ptr)
+{
+    serve_file(sockfd, WEB_ROOT "http.php");
+    serve_file(sockfd, WEB_ROOT "https.php");
+    serve_php(sockfd, "some_file.php");
+}
+
+void serve_connection_proxy(int sockfd, struct sockaddr_in *client_addr_ptr)
+{
+    char buffer[BUFFER_SIZE];
+    ssize_t num_bytes;
+    while ((num_bytes = read(sockfd, buffer, BUFFER_SIZE)) > 0)
+        send(sockfd, buffer, num_bytes, 0);
+    close(sockfd);
+}
+
+int main(int argc, char const *argv[])
+{
+    int sockfd, newsockfd;
+    struct sockaddr_in serv_addr, cli_addr;
+    FILE *fp;
+    char buffer[256];
+
+    // Créez un socket pour écouter les connexions entrantes
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        perror("Erreur lors de l'ouverture d'un socket");
+
+    // Initialisation du serveur et traitement des requêtes HTTP
+
+    // Initialisation du serveur
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(SERVER_PORT);
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        perror("Erreur lors de la liaison du socket");
+
+    // Lancement du serveur
+    listen(sockfd, 5);
+    while (1)
+    {
+        socklen_t clilen;
+        newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        if (newsockfd < 0)
+            perror("accept");
+        handle_connection(newsockfd, &cli_addr);
+    }
+    close(sockfd);
+    return 0;   
 }
